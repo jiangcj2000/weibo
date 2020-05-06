94d53329d8f7551c9b985066d73e2cbaae8c1015
jiang <jiangcj2000@gmail.com>
2020-05-06 23:46:49 +0900

    initialize style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-  </head>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
 
-  <body>
-    @include('layouts._header')
-
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <div class="offset-md-1 col-md-10">
-        @yield('content')
-        @include('layouts._footer')
-      </div>
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+            <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
     </div>
-  </body>
-</html>
+</nav>
+
+<div class="container">
+    @yield('content')
+</div>
+</body>
+s
