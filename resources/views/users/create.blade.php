08b88aaf6327edbc27ab4f6eff847847d39908cb
jiang <jiangcj2000@gmail.com>
2020-05-13 01:37:45 +0900

    User registration form.

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,36 @@
 @section('title', 'Registration')
 
 @section('content')
-<h1>Registration</h1>
+    <div class="offset-md-2 col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <h5>Registration</h5>
+            </div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('users.store') }}">
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="text" name="name" class="form-control" value="{{ old('email') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">Password Confirmation:</label>
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                    </div>
+
+                    <button class="btn btn-primary" type="submit">OK</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @stop
