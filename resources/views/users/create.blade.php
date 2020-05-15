83594b07bdf90f803084e82b660ecf06f2db1802
jiang <jiangcj2000@gmail.com>
2020-05-16 01:33:01 +0900

    test

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,7 +5,7 @@
     <div class="offset-md-2 col-md-8">
         <div class="card">
             <div class="card-header">
-                <h5>Registration</h5>
+                <h5>注册</h5>
             </div>
             <div class="card-body">
 
