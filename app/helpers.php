b6aef81bcc94bc711febf2217af8793753ceeebe
jiang <jiangcj2000@gmail.com>
2020-05-16 12:03:27 +0900

    correction errors.

--- app/helpers.php
+++ app/helpers.php
@@ -2,7 +2,7 @@
 
 function get_db_config(){
     if (getenv('IS_IN_HEROKU')){
-        $url = parse_ini_file(getenv("DATABASE_URL"));
+        $url = parse_url(getenv("DATABASE_URL"));
 
         return $db_config = [
             'connection' => 'pgsql',
